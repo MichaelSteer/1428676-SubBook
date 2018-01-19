7822862088e6cad72bbcb31a218b7bd9f46d37c4
MichaelSteer <swedester44@gmail.com>
2018-01-18 19:57:04 -0700

    Started Development of the UI - Added Subscription listview - Added second activity for adding a subscription

---
+++ app/src/main/java/com/steers/subbook/NewSubscriptionActivity.java
@@ -0,0 +1,13 @@
+package com.steers.subbook;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NewSubscriptionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_subscription);
+    }
+}
